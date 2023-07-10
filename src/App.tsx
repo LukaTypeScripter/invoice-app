00cafbde3c504b9d785bc0ac365403852641403d
luka shinjiakshvili <lukashinjikashvili85@gmail.com>
2023-07-10 17:08:56 +0400

    added rotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState,useContext } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import styled from 'styled-components'
 import './App.css'
@@ -7,12 +8,19 @@ import { DarkModeContext } from './contexts'
 
 function App() {
  const {darkMode} = useContext(DarkModeContext)
-
+//Todo:be able to go specific invoices with id invoice/:id
+//make add new invoice modal
+//filter modal and filter functionality
   return (
+    <Router>
     <AppCont darkMode={darkMode}>
-<SideNav />
-<Invoices />
+      <SideNav />
+      <Routes>
+        <Route   path="/" element={<Invoices/>} />
+      </Routes>
     </AppCont>
+  </Router>
+    
   )
 }
 
@@ -20,6 +28,7 @@ function App() {
 const AppCont = styled.div<{darkMode:boolean}>`
   background-color: ${props => props.darkMode ? "#141625" : "#f2f2f2"};
   display: flex;
+  color:white!important;
   gap: 36px;
   height: auto;
   min-height: 100vh;
