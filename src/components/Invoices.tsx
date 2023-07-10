7481a3d4e96922a5784b53d422689f47e17d544f
luka shinjiakshvili <lukashinjikashvili85@gmail.com>
2023-07-10 17:09:47 +0400

    mapping data

--- src/components/Invoices.tsx
+++ src/components/Invoices.tsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { ArrowImg, FillterTitle, FillterWrap, InvCont, InvFillterCont, InvTitle, InvTopCont, InvoicesComp, NewBtn, Plus, Subtitle, Title } from './styles/invoices'
+import React, { useContext } from 'react'
+import { ArrowImg, DotGreen, FillterTitle, FillterWrap, Info, InvCont, InvFillterCont, InvTitle, InvTopCont, InvoiceLeft, InvoiceMoney, InvoiceRigth, InvoicesComp, Links, List, ListItem, NewBtn, PaidText, Plus, StatusCont, StatusWrap, Subtitle, Title } from './styles/invoices'
 import { Pluss, arrowDown } from '../images'
+import { ListContext } from '../contexts'
 
 function Invoices() {
+    const {data} = useContext(ListContext)
   return (
     <InvoicesComp>
         <InvCont>
             <InvTopCont>
                 <InvTitle>
                     <Title>Invoices</Title>
-                    <Subtitle>There are 7 total invoices</Subtitle>
+                    <Subtitle>7 invoices</Subtitle>
                 </InvTitle>
                 <InvFillterCont>
                     <FillterWrap>
-                        <FillterTitle>Filter by status</FillterTitle>
+                        <FillterTitle>Filter</FillterTitle>
                             <ArrowImg src={arrowDown}/>
                     </FillterWrap>
                     <NewBtn>
@@ -23,6 +25,31 @@ function Invoices() {
                 </InvFillterCont>
                
             </InvTopCont>
+            <List>
+            {data.map((invoice) => (
+        <ListItem key={invoice.id}>
+          <Links to={`/invoices/${invoice.id}`}>
+            <InvoiceLeft>
+              <Info>{invoice.clientName}</Info>
+              <Info>{invoice.createdAt}</Info>
+              <Info>{invoice.description}</Info>
+            </InvoiceLeft>
+            <InvoiceRigth>
+              <InvoiceMoney>{invoice.total}</InvoiceMoney>
+              <StatusCont>
+               
+                <StatusWrap status={invoice.status}>
+                  
+                    <DotGreen status={invoice.status}/>
+                 
+                  <PaidText status={invoice.status}>{invoice.status}</PaidText>
+                </StatusWrap>
+              </StatusCont>
+            </InvoiceRigth>
+          </Links>
+        </ListItem>
+      ))}
+            </List>
         </InvCont>
     </InvoicesComp>
   )
