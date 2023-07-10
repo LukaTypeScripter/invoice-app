ba8dbc9ae53a1988c0e161e2635a5e788ad092e7
luka shinjiakshvili <lukashinjikashvili85@gmail.com>
2023-07-10 17:09:12 +0400

    show data

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import  { DarkModeContextProvider } from './contexts/DarkMode.tsx'
+import { ListContextProvider } from './contexts/List.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <DarkModeContextProvider>
+      <ListContextProvider>
     <App />
+    </ListContextProvider>
     </DarkModeContextProvider>
   </React.StrictMode>,
 )
