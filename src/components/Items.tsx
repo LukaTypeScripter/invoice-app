6f32fd0018ee99bee01e31c805ca361b276f7a18
luka shinjiakshvili <lukashinjikashvili85@gmail.com>
2023-07-11 18:36:06 +0400

    refactored

---
+++ src/components/Items.tsx
@@ -0,0 +1,56 @@
+
+import { Items, ItemsTable, ItemsTableName, ItemsTableTitle, ItemsTableWrap, TotalCOnt, TotalLabel } from './styles/specific'
+
+import { ItemProps } from './interfaces'
+
+function ItemsComp({info}:ItemProps) {
+  return (
+    <>
+    <Items>
+    
+    <ItemsTable>
+    <ItemsTableWrap>
+    
+<ItemsTableTitle>Item Name</ItemsTableTitle>
+
+    {info.items.map(item =>(
+        <>
+<ItemsTableName key={item.name}>{item.name}</ItemsTableName>
+        </>
+))}
+</ItemsTableWrap>
+<ItemsTableWrap>
+<ItemsTableTitle>QTI</ItemsTableTitle>
+    {info.items.map(item =>(
+        <>
+<ItemsTableName key={item.quantity}>{item.quantity}</ItemsTableName>
+        </>
+))}
+</ItemsTableWrap>
+<ItemsTableWrap>
+<ItemsTableTitle>Item price</ItemsTableTitle>
+    {info.items.map(item =>(
+        <>
+<ItemsTableName key={item.name}>{item.price}</ItemsTableName>
+        </>
+))}
+</ItemsTableWrap>
+<ItemsTableWrap>
+<ItemsTableTitle>Item total</ItemsTableTitle>
+    {info.items.map(item =>(
+        <>
+<ItemsTableName key={item.name}>{item.total}</ItemsTableName>
+        </>
+))}
+</ItemsTableWrap>
+</ItemsTable>
+<TotalCOnt>
+<TotalLabel>Amount Due</TotalLabel>
+    <TotalLabel>$ {info.items.reduce((total, item) => total + item.total, 0).toFixed(2)}</TotalLabel>
+</TotalCOnt>
+</Items>
+    </>
+  )
+}
+
+export default ItemsComp
